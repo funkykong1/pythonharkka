0579fa74c739289ebf94e34ef3053e413c9baca1
funkykong1 <vilho.valkeala@gmail.com>
2025-11-16 18:05:58 +0200

    fetch all ja fetchone toimii eritavalla ja minä haluan hypätä ikkunasta !!!!

--- mod13/teht02I-C-A-O.py
+++ mod13/teht02I-C-A-O.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def moikka():
     return "Hello world!!"
+icao = "efhk"
 
-
-# reitti on http://127.0.0.1:3000/alkuluku/luku
+# reitti on http://127.0.0.1:5000
 @app.route('/kentta/<icao>')
 def kaakao(icao):
 
@@ -25,8 +25,9 @@ def kaakao(icao):
 
     cursor = yhteys.cursor()
     # HAE country sekä airport hakemistoista kaikki relevantti tieto lentokentästä.
-    cursor.execute(f"SELECT airport.name, airport.municipality, FROM airport WHERE ident = {icao}")
-    lk = cursor.fetchall()
+    cursor.execute(f"select name, municipality from airport where ident = '{icao}'")
+    lk = cursor.fetchone()
+
 
     name = lk[0]
     region = lk[1]
